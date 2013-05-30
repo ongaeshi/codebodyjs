7bdc73be981827747ad8a4508af4fa63e0254c58
ongaeshi <ongaeshi0621@gmail.com>
2013-05-31 01:03:49 +0900

    Add enchant.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 // main.js
 
 $(function(){
-  (function() {
-    console.info("1");
-  }());
+  // console.info(1, 2, 3);
 });
+
+//------------------------------------------------------------------------------
+enchant();
+
+window.onload = function(){
+  var game = new Game(800, 480);
+
+  var label = new Label('Hello, enchant.js!');
+  game.rootScene.addChild(label);
+
+  game.start();
+}
