bd0470669e51426d1b03a13992ab72a3abc94a0f
ongaeshi <ongaeshi0621@gmail.com>
2013-05-31 02:16:50 +0900

    Code cleanup

--- js/main.js
+++ js/main.js
@@ -2,39 +2,41 @@
 
 var SCREEN_WIDTH  = 800;
 var SCREEN_HEIGHT = 480;
-
-var sGame;
-
-$(function(){
-
-  // console.info(1, 2, 3);
-
-  $("#button-run").click(function(){
-    try {
-      var result = eval($("textarea").val());
-
-      var label = new Label(result);
-      label.x = Math.random() * SCREEN_WIDTH;
-      label.y = Math.random() * SCREEN_HEIGHT;
-      sGame.rootScene.addChild(label);
-
-    } catch (e) {
-      if (e instanceof SyntaxError) {
-        alert(e.message);
-      }
-    }
-  });
-});
+var OBJECT_MARGIN = 50;
 
 //------------------------------------------------------------------------------
-enchant();
-enchant.ENV.PREVENT_DEFAULT_KEY_CODES = []
+$(function(){
+  var sGame = null;             // enchange's game object
 
-window.onload = function(){
-  sGame = new Game(SCREEN_WIDTH, SCREEN_HEIGHT);
+  function initEnchant(){
+    enchant();
+    enchant.ENV.PREVENT_DEFAULT_KEY_CODES = []
 
-  var label = new Label('Hello, enchant.js!');
-  sGame.rootScene.addChild(label);
+    window.onload = function(){
+      sGame = new Game(SCREEN_WIDTH, SCREEN_HEIGHT);
+      sGame.start();
+    }
+  }
 
-  sGame.start();
-}
+  function setupEvent(){
+    $("#button-run").click(function(){
+      try {
+        var result = eval($("textarea").val());
+
+        var label = new Label(result);
+        label.x = Math.random() * (SCREEN_WIDTH - OBJECT_MARGIN) + OBJECT_MARGIN;
+        label.y = Math.random() * (SCREEN_HEIGHT - OBJECT_MARGIN) + OBJECT_MARGIN;
+        sGame.rootScene.addChild(label);
+
+      } catch (e) {
+        if (e instanceof SyntaxError) {
+          alert(e.message);
+        }
+      }
+    });
+  }
+
+  //------------------------------------------------------------------------------
+  initEnchant();
+  setupEvent();
+});
